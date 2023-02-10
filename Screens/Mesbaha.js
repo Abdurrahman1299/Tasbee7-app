0d64f953bc9f3cbd9c4495f9612311cdd267c171
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-02-10 17:02:55 +0200

    feb-10

--- Screens/Mesbaha.js
+++ Screens/Mesbaha.js
@@ -1,20 +1,47 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 
 const Mesbaha = () => {
-  const [counts, setCounts] = useState(0);
+  const [counts, setCounts] = useState(99);
+
+  const countComponent = () => {
+    if (counts == 100) {
+      return <Text style={styles.oneHundred}>{counts}</Text>;
+    } else if (counts == 200) {
+      return <Text style={styles.twoHundred}>{counts}</Text>;
+    } else {
+      return <Text style={styles.ordinary}>{counts}</Text>;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.counts}>{counts}</Text>
-      <View>
-        <Pressable onPress={() => setCounts(counts + 1)}>
-          <Text style={styles.button}>Count</Text>
-        </Pressable>
-        <Pressable onPress={() => setCounts(0)}>
-          <Text style={styles.button}>Reset</Text>
-        </Pressable>
+    <ImageBackground
+      style={styles.background}
+      source={require("../img/background.jpg")}
+      resizeMode="cover"
+      opacity={0.72}
+    >
+      <View style={styles.container}>
+        {countComponent()}
+        <View>
+          <Pressable onPress={() => setCounts(counts + 1)}>
+            <Text style={styles.button}>Count</Text>
+          </Pressable>
+          <Pressable onPress={() => setCounts(counts - 1)}>
+            <Text style={styles.button}>minus</Text>
+          </Pressable>
+          <Pressable onPress={() => setCounts(0)}>
+            <Text style={styles.button}>Reset</Text>
+          </Pressable>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -27,8 +54,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-evenly",
   },
-  counts: {
+  background: {
+    flex: 1,
+  },
+  ordinary: {
     fontSize: 32,
+    color: "gold",
+  },
+  oneHundred: {
+    fontSize: 32,
+    color: "silver",
+  },
+  twoHundred: {
+    fontSize: 32,
+    color: "tomato",
+    fontWeight: "bold",
   },
   button: {
     marginTop: 18,
